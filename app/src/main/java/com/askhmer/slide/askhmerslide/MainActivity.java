c170b9f782f441bc3cba6b3db91ca7fe69142f92
Longdy <longdygold@gmail.com>
2016-06-10 10:14:13 +0700

    Initial commit

---
+++ app/src/main/java/com/askhmer/slide/askhmerslide/MainActivity.java
@@ -0,0 +1,13 @@
+package com.askhmer.slide.askhmerslide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
